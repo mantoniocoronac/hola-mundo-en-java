1a47f5fa238e952d15838f4f6dfe9b9ae8258d36
marco <alum.mantoniocoronc@iesalixar.org>
2020-09-24 15:30:11 +0200

    colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,12 @@
 public class HolaMundo {
 
-	public static void main(String[] args) {		
-		System.out.println("Hola");
-		System.out.println("Mundo");
+	public static void main(String[] args) {	
+	
+		String naranja = "\033[33m";
+		String azul = "\033[34m";
+			
+		System.out.println(naranja+"Hola");
+		System.out.println(azul+"Mundo");
 	}
 
 }
